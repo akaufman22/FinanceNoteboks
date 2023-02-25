3c953b5a798c0aaa9e687776d1e365f4d01e6c17
Alex Kaufman <el-diez@yandex.ru>
2023-02-25 10:45:06 +0000

    new commit

---
+++ OptionGreeks/VanillaOption.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 00:08:57 2023
+
+@author: alex
+"""
+
+from bsm_math import *
+
+
+class VanillaOption():
+
+    def __init__(self, K, T, Flag):
+        self.K = K
+        self.T = T
+        self.Flag = Flag
+
+    def bs_value(self, S, r, q, sigma):
+        return bsm_value(S, self.K, self.T, r, q, sigma, self.Flag)
+
+    def bs_vega(self, S, r, q, sigma):
+        return bsm_vega(S, self.K, self.T, r, q, sigma)
+
+    def bs_delta(self, S, r, q, sigma):
+        return bsm_delta(S, self.K, self.T, r, q, sigma, self.Flag)
+
+    def bs_delta_prem(self, S, r, q, sigma):
+        return bsm_delta_prem(S, self.K, self.T, r, q, sigma, self.Flag)
+
+    def bs_ivol(self, S, r, q, V, sigma_est=0.3, it=100, tol=0.001):
+        return bsm_ivol(S, self.K, self.T, r, q, V, self.Flag, sigma_est,
+                        it=100, tol=0.001)
---
+++ OptionGreeks/bsm_math.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 16:50:30 2023
+
+@author: alex
+"""
+
+
+def bsm_value(S, K, T, r, q, sigma, Flag):
+    from math import log, sqrt, exp
+    from scipy import stats
+
+    S = float(S)
+    K = float(K)
+    d1 = (log(S/K) + (r - q + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
+    d2 = (log(S/K) + (r - q - 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
+    if Flag == 0:
+        value = (S * exp(-q * T) * stats.norm.cdf(d1, 0.0, 1.0) -
+                 K * exp(-r * T) * stats.norm.cdf(d2, 0.0, 1.0))
+    elif Flag == 1:
+        value = (K * exp(-r * T) * stats.norm.cdf(-d2, 0.0, 1.0) -
+                 S * exp(-q * T) * stats.norm.cdf(-d1, 0.0, 1.0))
+    else:
+        value = 'NaN'
+    return value
+
+
+def bs76_value(F, K, T, r, sigma, Flag):
+    from math import log, sqrt, exp
+    from scipy import stats
+
+    F = float(F)
+    K = float(K)
+    d1 = (log(F/K) + (0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
+    d2 = d1 - sigma * sqrt(T)
+    if Flag == 0:
+        value = (F * exp(-r * T) * stats.norm.cdf(d1, 0.0, 1.0) -
+                 K * exp(-r * T) * stats.norm.cdf(d2, 0.0, 1.0))
+    elif Flag == 1:
+        value = (K * exp(-r * T) * stats.norm.cdf(-d2, 0.0, 1.0) -
+                 F * exp(-r * T) * stats.norm.cdf(-d1, 0.0, 1.0))
+    else:
+        value = 'NaN'
+    return value
+
+
+def bsm_vega(S, K, T, r, q, sigma):
+    from math import log, sqrt
+    from scipy import stats
+    S = float(S)
+    K = float(K)
+    d1 = (log(S/K) + (r - q + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
+    vega = S * stats.norm.pdf(d1, 0.0, 1.0) * sqrt(T)
+    return vega
+
+
+def bsm_delta(S, K, T, r, q, sigma, Flag):
+    from math import log, sqrt, exp
+    from scipy import stats
+    S = float(S)
+    K = float(K)
+    d1 = (log(S/K) + (r - q + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
+    if Flag == 0:
+        delta = exp(-q * T) * stats.norm.cdf(d1, 0.0, 1.0)
+    elif Flag == 1:
+        delta = exp(-q * T) * (stats.norm.cdf(d1, 0.0, 1.0) - 1)
+    else:
+        delta = 'NaN'
+    return delta
+
+
+def bsm_delta_prem(S, K, T, r, q, sigma, Flag):
+    delta = (bsm_delta(S, K, T, r, q, sigma, Flag) -
+             bsm_value(S, K, T, r, q, sigma, Flag) / S)
+    return delta
+
+
+def bsm_ivol(S, K, T, r, q, V, Flag, sigma_est, it=100, tol=0.001):
+    for i in range(it):
+        sigma_prev = sigma_est
+        sigma_est -= ((bsm_value(S, K, T, r, q, sigma_est, Flag) - V) /
+                      bsm_vega(S, K, T, r, q, sigma_est))
+        if abs(sigma_est - sigma_prev) < tol:
+            break
+    return sigma_est
